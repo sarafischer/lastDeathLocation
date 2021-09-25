8ef3ec04a087f6b77608e1816fcbceda4804ecb0
Chridoure <73531823+Chridoure@users.noreply.github.com>
2021-09-25 12:36:39 +0200

    main class now upperCase

--- src/main/java/com/Timbuktu.java
+++ src/main/java/com/Timbuktu.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLOutput;
 
-public final class timbuktu extends JavaPlugin {
+public final class Timbuktu extends JavaPlugin {
 
     @Override
     public void onEnable() {
